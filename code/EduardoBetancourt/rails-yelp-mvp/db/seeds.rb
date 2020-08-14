73a6fd29a7aed033c0771a92120be353bf7375bb
Eduardo Betancourt <ebhdz@hotmail.com>
2020-08-14 11:10:09 -0500

    rails new

--- code/EduardoBetancourt/rails-yelp-mvp/db/seeds.rb
+++ code/EduardoBetancourt/rails-yelp-mvp/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+200.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  phone_number = Faker::PhoneNumber.cell_phone
+  category = %w[chinese italian japanese french belgian].sample
+  restaurant = Restaurant.new({ name: name, address: address, phone_number: phone_number, category: category })
+  restaurant.save
+
+  500.times do
+    review = Review.new({ rating: rand(6), content: Faker::Restaurant.review, restaurant: restaurant })
+    review.save
+  end
+end
